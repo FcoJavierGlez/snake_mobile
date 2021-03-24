2d7eaca8b67d06bf11205cb837b72ae7391ecc00
FcoJavierGlez <FJGS_86@hotmail.com>
2021-03-24 10:37:09 +0100

    Updated pruebas.js file

--- js/pruebas.js
+++ js/pruebas.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener("click", e => {
         console.log(`Ancho: ${e.pageX}`);
         console.log(`Alto: ${e.pageY}`);
-        if (e.pageY < innerHeight * 0.3 || e.pageY > innerHeight * 0.6) 
+        if (e.pageY < innerHeight * 0.3 || e.pageY > innerHeight * 0.7) 
             info.style.backgroundColor = e.pageY < innerHeight * 0.3 ? "red" : "blue";
         else
             info.style.backgroundColor = e.pageX < innerWidth * 0.5 ? "green" : "yellow";
