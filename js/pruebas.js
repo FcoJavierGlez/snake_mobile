7a96725d546c06233b5e9e430b11560ef36a55fb
FcoJavierGlez <FJGS_86@hotmail.com>
2021-03-24 10:23:37 +0100

    Updated pruebas.js

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const info = document.getElementsByClassName("info")[0];
 
-    document.addEventListener("click", () => {
-        info.style.backgroundColor = "red";
+    document.addEventListener("click", e => {
+        /* console.log(`Ancho: ${e.pageX}`);
+        console.log(`Alto: ${e.pageY}`); */
+        if (e.pageY < outerHeight * 0.3 || e.pageY > outerHeight * 0.6) 
+            info.style.backgroundColor = e.pageY < outerHeight * 0.3 ? "red" : "blue";
+        else
+            info.style.backgroundColor = e.pageX < outerWidth * 0.5 ? "green" : "yellow";
     });
 });
