fee436a6326efb3f7caccf0fda822e28bd9dd4e9
FcoJavierGlez <FJGS_86@hotmail.com>
2021-03-24 10:47:54 +0100

    Updated CSS and pruebas.js file

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const info = document.getElementsByClassName("info")[0];
+    const boardgame = document.getElementsByClassName("render_boardgame")[0];
 
     document.addEventListener("click", e => {
-        console.log(`Ancho: ${e.pageX}`);
-        console.log(`Alto: ${e.pageY}`);
+        /* console.log(`Ancho: ${e.pageX}`);
+        console.log(`Alto: ${e.pageY}`); */
         if (e.pageY < innerHeight * 0.3 || e.pageY > innerHeight * 0.7) 
-            info.style.backgroundColor = e.pageY < innerHeight * 0.3 ? "red" : "blue";
+            boardgame.style.backgroundColor = e.pageY < innerHeight * 0.3 ? "red" : "blue";
         else
-            info.style.backgroundColor = e.pageX < innerWidth * 0.5 ? "green" : "yellow";
+            boardgame.style.backgroundColor = e.pageX < innerWidth * 0.5 ? "green" : "yellow";
     });
 });
