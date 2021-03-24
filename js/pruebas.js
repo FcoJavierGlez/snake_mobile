7ea400908d25de216ec8fe095588c5d86f197512
FcoJavierGlez <FJGS_86@hotmail.com>
2021-03-24 12:01:26 +0100

    Updated pruebas.js

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const boardgame = document.getElementsByClassName("render_boardgame")[0];
 
-    document.addEventListener("click", e => {
-        /* console.log(`Ancho: ${e.pageX}`);
-        console.log(`Alto: ${e.pageY}`); */
+    /* document.addEventListener("click", e => {
         if (e.pageY < innerHeight * 0.3 || e.pageY > innerHeight * 0.7) 
             boardgame.style.backgroundColor = e.pageY < innerHeight * 0.3 ? "red" : "blue";
         else
             boardgame.style.backgroundColor = e.pageX < innerWidth * 0.5 ? "green" : "yellow";
-    });
+    }); */
 });
